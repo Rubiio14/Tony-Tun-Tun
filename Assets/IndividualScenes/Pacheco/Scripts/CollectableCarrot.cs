6321b08b162ae2f9bb1e589cfa74ca90fc8ab770
mpacdia <mpacdia@gmail.com>
2024-12-05 09:49:25 +0000

    cambiadas cosas en la recoleccion de objetos
    no puedo continuar porque necesito que la estamina esté hecha para continuar

--- Assets/IndividualScenes/Pacheco/Scripts/CollectableCarrot.cs
+++ Assets/IndividualScenes/Pacheco/Scripts/CollectableCarrot.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
-using static CollectablesManager;
 
 
 public class CollectableCarrot : MonoBehaviour, ICollectable
 {
-   public void collect()
-    {
 
+    //ID para identificarla
+    public int totalCarrot = 0;
+
+   public void Collect()
+   {
+        //Hace un sonido especifico, un efecto de particulas o una animación
+        //Manage.Instance y sumar 1 zanahoria, que le dice cual cogio basado en la ID 
+        totalCarrot++;
+        Destroy(gameObject);
     }
+
 }
