5bb065e83be25c1808aa6e2c8c403ef080036712
lpjunity <lpjunity25@gmail.com>
2025-02-09 22:39:50 +0000

    Small fix

--- Assets/IndividualScenes/Laura/Scripts/MainMenuUIController.cs
+++ Assets/IndividualScenes/Laura/Scripts/MainMenuUIController.cs
@@ -88,6 +88,10 @@ public class MainMenuUIController : MonoBehaviour
         {
             UIManager.Instance.SaveAndQuit();
         }
+        else
+        {
+            Application.Quit();
+        }
     }
 
 }
